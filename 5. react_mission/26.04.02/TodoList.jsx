2b309fdfeb9b728e9d3ff330e74a2576e001cd1c
01218279393 <478lemontree@gmail.com>
2026-04-04 23:56:09 +0900

    react practice

--- 5. react_mission/26.04.02/TodoList.jsx
+++ 5. react_mission/26.04.02/TodoList.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 
-function Todolist() {
-  return <div></div>;
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [lists, setLists] = useState([]);
+  const inputRef = useRef();
+
+  const AddTodo = () => {
+    if (!todo.trim()) return;
+    setLists([...lists, { id: Date.now(), text: todo, isDone: false }]);
+    setTodo("");
+  };
+
+  const toggleTodo = (id) => {
+    setLists(
+      lists.map((item) =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item,
+      ),
+    );
+  };
+
+  const deleteList = (id) => {
+    setLists(lists.filter((item) => item.id !== id));
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [lists]);
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={AddTodo}>추가</button>
+
+      {lists.map((item) => (
+        <div key={item.id}>
+          <input type="checkbox" onClick={() => toggleTodo(item.id)} />
+          {item.isDone ? <s>{item.text}</s> : item.text}
+          <button onClick={() => deleteList(item.id)}>삭제</button>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default Todolist;
